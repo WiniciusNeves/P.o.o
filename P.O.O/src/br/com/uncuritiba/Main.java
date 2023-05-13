39ad89d44bfa372edd93d845918a306c3a1423fb
Samu <127152668+WiniciusNeves@users.noreply.github.com>
2023-05-12 21:59:31 -0300

    Update Main.java

--- P.O.O/src/br/com/uncuritiba/Main.java
+++ P.O.O/src/br/com/uncuritiba/Main.java
@@ -13,32 +13,47 @@ public class Main {
 			String Contato = null;
 			String Matricula = null;
 			Scanner scanner = new Scanner(System.in);
-			
+		
 			System.out.println("cadastro de Prof ");
 			System.out.println("");
 			System.out.println("");
+			System.out.println("cadastro de Aluno ");
+			int numeroAlunos = scanner.nextInt();
+			scanner.nextLine();
+			Aluno[] alunos = new Aluno[numeroAlunos];
+			
+			for(int i = 0; i<numeroAlunos;i++) {
+				
+				System.out.print("Informe quantos alunos estaram presentes na turma: \n");
+				
+				Scanner scanner1 = new Scanner(System.in);
+				int nalunos = scanner1.nextInt();
+				
+				for (int x = 0; x > nalunos; x++)
+				{
+					Aluno aluno = new Aluno();
+					aluno.setNome(Nome);
+					Nome = scanner.nextLine();
+					aluno.setCPF(CPF);
+					CPF = scanner.nextLine();
+					aluno.setEmail(Email);
+					Email = scanner.nextLine();
+					aluno.setContato(Contato);
+					Contato = scanner.nextLine();
 			
 			Prof professor = new Prof();
 			professor.setNome(Nome);
 			professor.setMatricula(Matricula);
 			professor.setEmail(Email);
 				
-			System.out.print("Informe quantos alunos estaram presentes na turma: \n");
-			
-			Scanner scanner1 = new Scanner(System.in);
-			int nalunos = scanner1.nextInt();
-			
-			for (int x = 0; x > nalunos; x++)
-			{
-				Aluno aluno = new Aluno();
-				aluno.setNome(Nome);
-				aluno.setCPF(CPF);
-				aluno.setEmail(Email);
-				aluno.setContato(Contato);
+			alunos[i] = aluno;
 					
 			}
 		
+				System.out.println(alunos[0].getNome());
 					
+					
+		}	
 	}
 	
 
